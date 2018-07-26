7d678aa5a9b2bc330de4cf815f420f7dc9f4e9b4
wohenkuaihuo <471404293@qq.com>
2018-07-26 18:57:24 +0800

    抽象工厂模式

--- src/com/baniao/abstrFactoryMethod/shixian/NotebookComputerA.java
+++ src/com/baniao/abstrFactoryMethod/shixian/NotebookComputerA.java
@@ -2,9 +2,9 @@ package com.baniao.abstrFactoryMethod.shixian;
 
 import com.baniao.abstrFactoryMethod.jiekou.NotebookComputer;
 
-public class NotebookComputerA extends NotebookComputer {
-@Override
-public void doUse() {
-	System.out.println("这是个人笔记本的功能A");
-}
-}
+	public class NotebookComputerA extends NotebookComputer {
+	@Override
+	public void doUse() {
+		System.out.println("这是个人笔记本的功能A");
+	}
+	}
